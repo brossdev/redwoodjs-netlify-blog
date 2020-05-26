493cb81318a72b09f01be0d8527d19926b09c948
Bobby <Bobby1985uk@gmail.com>
2020-05-26 19:13:53 +0100

    update home page to include links

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,20 @@
+import { Link, routes } from '@redwoodjs/router'
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-    </div>
+    <>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
   )
 }
 
