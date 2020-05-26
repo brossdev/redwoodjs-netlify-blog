597cbb318298eef745d348f9abecdc00ece2752e
Bobby <Bobby1985uk@gmail.com>
2020-05-26 19:44:14 +0100

    feat: add routing to about page

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,26 @@
+import { Link, routes } from '@redwoodjs/router'
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>AboutPage</h1>
-      <p>Find me in ./web/src/pages/AboutPage/AboutPage.js</p>
-    </div>
+    <>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+        <Link to={routes.home()}>Return Home</Link>
+      </main>
+    </>
   )
 }
 
